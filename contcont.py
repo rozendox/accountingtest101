ac47de6771d86948312d58b23f986f4fc194c7b6
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-05-17 16:56:26 -0300

    Update contcont.py

--- contcont.py
+++ contcont.py
@@ -3,10 +3,10 @@ import sqlite3
 
 class Cont:
     def __init__(self):
-        self.conn = sqlite3.connect("contabilidadeteste2.db")
+        self.conn = sqlite3.connect("contabilidadeteste1.db")
         self.cursor = self.conn.cursor()
 
-    def adicionar_dados_seg(self):
+    def adicionar_dados_seg(self, entrada, gastos):
         try:
             sql_query = f"INSERT INTO janeiro(entrada,gastos)Values ({entrada}, {gastos})"
             self.cursor.execute(sql_query)
@@ -14,3 +14,12 @@ class Cont:
             return "dados inseridos"
         except Exception as e:
             return f"Operação não-sucedida. Erro {e}"
+
+    def adicionar_dados_fev(self, entrada, gastos):
+        try:
+            sql_query = f"INSERT INTO fevereiro(entrada, gastos) Values ({entrada}, {gastos})"
+            self.cursor.execute(sql_query)
+            self.conn.commit()
+            return "Dados Inseridos"
+        except Exception as e:
+            return f"operação não sucedida. Erro {e}"
