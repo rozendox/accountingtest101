04a602a430e3835cdfd5d56db933bb38af9dd080
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-05-17 16:56:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import contcont
 
-sqlconnector = contcont
+sqlconnector = contcont.Cont()
 
 
 def main():
     print("\t\t\t\t@===========================================@")
     print("\t\t\t\t@===========-REPORT THE PASSWORD-===========@")
     print("\t\t\t\t@===========================================@")
-    senha = 9645
+    senha = 4587
     sen = int(input("..."))
     while sen != senha:
         print("\tvocê tem mais 3 tentaivas...\n")
@@ -43,21 +43,46 @@ def main():
         print("\t\t\t\t@===========-Welcome To the System!-===========@")
         print("\t\t\t\t@==============================================@")
 
-    def WITHDRAW():
-        entrada1 = int(input("Entry with the amount to add in entrada value "))
-        gastos2 = int(input("Entry with the amount to add in gastos value"))
+    def deposit_january():
+        entrada = int(input("Enter with the value to add to the Entry value"))
+        gastos = int(input("Enter the amount to be added to the cost amount"))
+        sqlconnector.adicionar_dados_seg(entrada, gastos)
 
-        def add(entrada1, gastos2):
-            entrada = entrada1
-            gastos = gastos2
-            sqlconnector.Cont.adicionar_dados_seg(entrada, gastos)
+    def deposit_february():
+        entrada = int(input("Enter with the value to add to the Entry value"))
+        gastos = int(input("Enter the amount to be added to the cost amount"))
+        sqlconnector.adicionar_dados_fev(entrada, gastos)
 
     print("\t1 - ADD values")
-    contador = int(input("\tPlease Select one:"))
+    counter = int(input("\tPlease Select one:"))
 
-    while contador != 0:
-        if contador == 1:
-            WITHDRAW()
+    while counter != 0:
+        if counter == 1:
+            print(":\t\tWICH MONTH DO YOU WANT TO PERFORM AN OPERATION??:")
+            print("\t1 - Janruary")
+            print("\t2 - February")
+            month = int(input("@@-INFORM THE DESIRED MONTH-@@"))
+            if month == 1:
+                deposit_january()
+                print("@@ DATA ENTERED SUCCESSFULLY @@")
+                print("@@ DO YOU WANT TO RETURN TO MAIN MENU? @@")
+                end = input("... Y/N")
+                if end.lower() == "y":
+                    main()
+                else:
+                    exit()
+            elif month == 2:
+                deposit_february()
+                print("@@ DATA ENTERED SUCCESSFULLY @@")
+                print("@@ DO YOU WANT TO RETURN TO MAIN MENU? @@")
+                end = input("... Y/N")
+                if end.lower() == "y":
+                    main()
+                else:
+                    exit()
+
+        elif counter == 2:
+            pass
 
 
 if __name__ == '__main__':
