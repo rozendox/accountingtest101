4e0bee5f1e53b0bf681e53966fe4a775864a0340
Gabriel Rozendo <79455923+rozendox@users.noreply.github.com>
2023-05-18 08:43:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import contcont
 
 sqlconnector = contcont.Cont()
+sqlconnectorver = contcont.ContVer()
 
 
 def main():
@@ -43,18 +44,30 @@ def main():
         print("\t\t\t\t@===========-Welcome To the System!-===========@")
         print("\t\t\t\t@==============================================@")
 
+# ----------------------------------------------------------------------------------------------------------------------
+# FIXME -> LINHA DE ENTRADA DE DADOS -----------------------------------------------------------------------------------
+
     def deposit_january():
         entrada = int(input("Enter with the value to add to the Entry value"))
         gastos = int(input("Enter the amount to be added to the cost amount"))
-        sqlconnector.adicionar_dados_seg(entrada, gastos)
-
+        result = sqlconnector.adicionar_dados_jan(entrada, gastos)
+        print(result)
     def deposit_february():
-        entrada = int(input("Enter with the value to add to the Entry value"))
-        gastos = int(input("Enter the amount to be added to the cost amount"))
+        entrada = float(input("Enter with the value to add to the Entry value"))
+        gastos = float(input("Enter the amount to be added to the cost amount"))
         sqlconnector.adicionar_dados_fev(entrada, gastos)
 
+    def deposit_mar():
+        entrada = int(input("Enter with the value to add to the Entry value"))
+        gastos = int(input("Enter the amount to be added to the cost amount"))
+        sqlconnector.adicionar_dados_mar(entrada, gastos)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# FIXME -> LINHA DE VIZUALIZAÇÃO DE DADOS ---------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------------------------------------
     print("\t1 - ADD values")
-    counter = int(input("\tPlease Select one:"))
+    print("\t2 - SEE values")
+    counter = int(input ("\tPlease Select one:"))
 
     while counter != 0:
         if counter == 1:
@@ -80,10 +93,37 @@ def main():
                     main()
                 else:
                     exit()
+            elif month == 3:
+                deposit_mar()
+                print("@@ DO YOU WANT TO RETURN TO MAIN MENU? @@")
+                end = input("... Y/N")
+                if end.lower() == "y":
+                    main()
+                else:
+                    exit()
 
         elif counter == 2:
-            pass
+            print(":\t\tWICH MONTH DO YOU WANT TO PERFORM AN OPERATION??:")
+            print("\t1 - Janruary")
+            print("\t2 - February")
+            month = int(input("@@-INFORM THE DESIRED MONTH-@@"))
+            if month == 1:
+                sqlconnectorver.return_jan
+                print("@@  SUCCESSFULLY VISUALIZED DATA @@")
+                print("@@ DO YOU WANT TO RETURN TO MAIN MENU? @@")
+                end = input("... Y/N")
+                if end.lower() == "y":
+                    main()
+                else:
+                    exit()
+            elif month == 2:
+                sqlconnectorver.return_fev
+                print("@@  SUCCESSFULLY VISUALIZED DATA @@")
+                print("@@ DO YOU WANT TO RETURN TO MAIN MENU? @@")
+                end = input("... Y/N")
+                if end.lower() == "y":
+                    main()
+                else:
+                    exit()
 
-
-if __name__ == '__main__':
-    main()
+main()
